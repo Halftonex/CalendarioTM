39ce818e790b3334d6a5db0204e43c6ae1676237
AlatarL <natryum@libero.it>
2020-12-02 10:41:31 +0100

    Test123

---
+++ CalendarioTM/Pages/Extensions.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CalendarioTM.Pages
+{
+    public static class Extensions
+    {
+        internal static readonly DateTime _abnormalBloom = new DateTime(2014, 1, 1);
+
+        public static readonly Dictionary<int, string> GregorianMonths = new Dictionary<int, string>()
+        {
+            [1] = "Gennaio",
+            [2] = "Febbraio",
+            [3] = "Marzo",
+            [4] = "Aprile",
+            [5] = "Maggio",
+            [6] = "Giugno",
+            [7] = "Luglio",
+            [8] = "Agosto",
+            [9] = "Settembre",
+            [10] = "Ottobre",
+            [11] = "Novembre",
+            [12] = "Dicembre"
+        };
+
+        public static readonly Dictionary<int, string> ImperialMonths = new Dictionary<int, string>()
+        {
+            [1] = "Postapritore",
+            [2] = "Forense",
+            [3] = "Macinale",
+            [4] = "Adulain",
+            [5] = "Madrigale",
+            [6] = "Granaio",
+            [7] = "Lithe",
+            [8] = "Antedain",
+            [9] = "Solfeggiante",
+            [10] = "Orifoglia",
+            [11] = "Nembonume",
+            [12] = "Dodecabrullo"
+        };
+
+        public static readonly Dictionary<int, string> QuenyaMonths = new Dictionary<int, string>()
+        {
+            [1] = "Narvinyë",
+            [2] = "Nénimë",
+            [3] = "Súlimë",
+            [4] = "Víressë",
+            [5] = "Lótessë",
+            [6] = "Nárië",
+            [7] = "Cermië",
+            [8] = "Úrimë",
+            [9] = "Yavannië",
+            [10] = "Narquelië",
+            [11] = "Hísimë",
+            [12] = "Ringarë",
+            [13] = "Arda"
+        };
+
+        public static readonly Dictionary<int, string> SindarMonths = new Dictionary<int, string>()
+        {
+            [1] = "Narwain",
+            [2] = "Nínui",
+            [3] = "Gwaeron",
+            [4] = "Gwirith",
+            [5] = "Lothron",
+            [6] = "Nórui",
+            [7] = "Cerveth",
+            [8] = "Úrui",
+            [9] = "Ivanneth",
+            [10] = "Narbeleth",
+            [11] = "Hithui",
+            [12] = "Girthron",
+            [13] = "Arda"
+        };
+
+        public static (int Year, int Month, int Day) ToImperialCount(this DateTime gregorian)
+        {
+            return (gregorian.Year - 1736, gregorian.Month, gregorian.Day);
+        }
+
+        public static (int Bloom, int Part, int Month, int Day) ToElvenDate(this DateTime gregorian)
+        {
+            int month = gregorian.DayOfYear / 30 + 1;
+            int day = gregorian.DayOfYear % 30;
+            if (day == 0)
+            {
+                day = 30;
+                month--;
+            }
+
+            double value = (gregorian.Year - 337) / 78;
+            int bloom = (int)Math.Round(value, MidpointRounding.AwayFromZero);
+            if (gregorian >= _abnormalBloom)
+            {
+                bloom++;
+                int part = (gregorian.Year - 2014) % 78 + 1;
+                return (bloom, part, month, day);
+            }
+            else
+            {
+                int part = (gregorian.Year - 337) % 78 + 1;
+                return (bloom, part, month, day);
+            }
+        }
+    }
+}
---
+++ CalendarioTM/Pages/Index.razor.cs
@@ -0,0 +1,182 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.JSInterop;
+using System;
+using static CalendarioTM.Pages.Extensions;
+
+namespace CalendarioTM.Pages
+{
+    public partial class Index
+    {
+        [Inject]
+        protected IJSRuntime JSRuntime { get; set; }
+
+        private string _abnormalBloomString => $"[fioritura anomala: {Extensions._abnormalBloom:dd/MM/yyyy}]";
+
+        private string _error;
+
+        private int _day = DateTime.Now.Day;
+        private int _month = DateTime.Now.Month;
+        private int _year = DateTime.Now.Year;
+        private string _gregorianString;
+
+        private int _dayIC;
+        private int _monthIC;
+        private int _yearIC;
+        private string _imperialString;
+
+        private int _dayE;
+        private int _monthE;
+        private int _partE;
+        private int _bloomE;
+        private string _quenyaString;
+        private string _sindarString;
+
+        protected override void OnInitialized()
+        {
+            FromGregorian();
+        }
+
+        private void SetError(string text)
+        {
+            _error = text;
+            _gregorianString = string.Empty;
+            _imperialString = string.Empty;
+            _quenyaString = string.Empty;
+            _sindarString = string.Empty;
+        }
+
+        private void FromGregorian()
+        {
+            if (DateTime.TryParse($"{_year}-{_month}-{_day}", out DateTime gregorianDate))
+            {
+                _error = string.Empty;
+                (_yearIC, _monthIC, _dayIC) = gregorianDate.ToImperialCount();
+                (_bloomE, _partE, _monthE, _dayE) = gregorianDate.ToElvenDate();
+
+                UpdateGregorianString();
+                UpdateImperialString();
+                UpdateQuenyaString();
+                UpdateSindarString();
+            }
+            else
+            {
+                SetError("Data non valida");
+            }
+        }
+
+        private void FromImperial()
+        {
+            _day = _dayIC;
+            _month = _monthIC;
+            _year = _yearIC + 1736;
+
+            FromGregorian();
+        }
+
+        private void FromElven()
+        {
+            if (elvenDateDoesntExist())
+            {
+                SetError("Data elfica inesistente per via della fioritura anomala");
+            }
+            else
+            {
+                DateTime date = FromElvenToGregorian(_bloomE, _partE, _monthE, _dayE);
+                if (InputWasGood(date))
+                {
+                    _error = string.Empty;
+                    _year = date.Year;
+                    _month = date.Month;
+                    _day = date.Day;
+                    FromGregorian();
+                }
+            }
+
+            // ↓↓ local functions ↓↓ //
+            bool elvenDateDoesntExist()
+            {
+                return _bloomE == 21 && _partE >= 39 && _monthE >= 13 && _dayE >= 5;
+            }
+
+            bool InputWasGood(DateTime date)
+            {
+                if (_monthE == 13 && _dayE > 6)
+                {
+                    SetError("Data elfica non valida");
+                    return false;
+                }
+                else if (_monthE == 13 && _dayE == 6 && date.Day != 31)
+                {
+                    SetError("Data elfica non bisestile");
+                    return false;
+                }
+                return true;
+            }
+        }
+
+        private DateTime FromElvenToGregorian(int bloom, int part, int month, int day)
+        {
+            int year = getYear();
+            int dayOfYear = (month - 1) * 30 + day;
+            return new DateTime(year, 1, 1).AddDays(dayOfYear - 1);
+
+            // ↓↓ local functions ↓↓ //
+            int getYear()
+            {
+                if (bloom >= 22)
+                { return bloom * 78 + part + 297; }
+                else
+                { return bloom * 78 + part + 336; }
+            }
+        }
+
+        private void UpdateGregorianString()
+        {
+            _gregorianString = $"{_day} {GregorianMonths[_month]} {_year}";
+        }
+
+        private void UpdateImperialString()
+        {
+            _imperialString = $"{_dayIC} {ImperialMonths[_monthIC]} {_yearIC}";
+        }
+
+        private void UpdateQuenyaString()
+        {
+            _quenyaString = $"{_dayE} {QuenyaMonths[_monthE]} {_partE}^ parte della {_bloomE}^ Fioritura";
+        }
+
+        private void UpdateSindarString()
+        {
+            _sindarString = $"{_dayE} {SindarMonths[_monthE]} {_partE}^ parte della {_bloomE}^ Fioritura";
+        }
+
+        private void CopyGregorianToClipboard() => CopyToClipboard(_gregorianString);
+        private void CopyImperialToClipboard() => CopyToClipboard(_imperialString);
+        private void CopyQuenyaToClipboard() => CopyToClipboard(_quenyaString);
+        private void CopySindarToClipboard() => CopyToClipboard(_sindarString);
+
+        private void CopyToClipboard(string text)
+        {
+            JSRuntime.InvokeVoidAsync("clipboardCopy.copyText", text);
+        }
+
+        private void FromGregorianKeyup(KeyboardEventArgs args)
+        {
+            if (args.Key == "Enter")
+                FromGregorian();
+        }
+
+        private void FromImperialKeyup(KeyboardEventArgs args)
+        {
+            if (args.Key == "Enter")
+                FromImperial();
+        }
+
+        private void FromElvenKeyup(KeyboardEventArgs args)
+        {
+            if (args.Key == "Enter")
+                FromElven();
+        }
+    }
+}
